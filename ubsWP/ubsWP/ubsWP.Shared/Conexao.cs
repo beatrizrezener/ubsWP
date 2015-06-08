2dcd6abd919beede1e3c370758328814e20b0d6c
unknown <beatriz.rezener@LAB-F26-04.lab.mic>
2015-06-08 20:27:23 -0300

    Criando classe de conexao

---
+++ ubsWP/ubsWP/ubsWP.Shared/Conexao.cs
@@ -0,0 +1,59 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace ubsWP
+{
+    class Conexao
+    {
+        static string nomeBase = Path.Combine(ApplicationData.Current.LocalFolder.Path,
+                                              "saudf.sqlite");
+
+        private static async Task<bool> VerificarBanco(string nomeBanco)
+        {
+            bool retorno = true;
+
+            try
+            {
+                await ApplicationData.Current.LocalFolder.GetFileAsync(nomeBanco);
+            }
+            catch (Exception)
+            {
+                retorno = false;
+            }
+
+            return retorno;
+        }
+
+        private static void CriarBaseDados()
+        {
+            //É importante usar o using pq ele já faz alguns tratamentos
+            using (var db = new SQLiteConnection(nomeBase))
+            {
+                db.CreateTable<Unidade>();
+            }
+        }
+        private static void CriarBase()
+        {
+            //bool bancoExiste = VerificarBanco(nomeBase).Result; // .Result para nao correr o risco do async nao ter terminado
+
+            // if(!bancoExiste) //A base tá criada? Se sim, sai, se não, cria.
+            //  {
+            CriarBaseDados();
+            // }
+        }
+
+        public static SQLiteConnection Conn()
+        {
+            CriarBase(); // Criar base
+
+            SQLiteConnection sConn = new SQLiteConnection(nomeBase); //Tenta fazer a conexão
+
+            return sConn;
+        }
+    }
+}

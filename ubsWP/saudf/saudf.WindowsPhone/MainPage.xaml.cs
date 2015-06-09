6e09e244f58a22ff56ce9a7eccd7f8a1bc72a2b2
unknown <beatriz.rezener@LAB-F26-04.lab.mic>
2015-06-09 19:38:48 -0300

    Mostrando localização corrente no mapa

--- ubsWP/saudf/saudf.WindowsPhone/MainPage.xaml.cs
+++ ubsWP/saudf/saudf.WindowsPhone/MainPage.xaml.cs
@@ -47,25 +47,44 @@ namespace saudf
         }
         private async void Button_Click(object sender, RoutedEventArgs e)
         {
-            Geolocator geolocator = new Geolocator();
-            geolocator.DesiredAccuracyInMeters = 50;
+            //Geolocator geolocator = new Geolocator();
+            //geolocator.DesiredAccuracyInMeters = 50;
 
+            //try
+            //{
+            //    Geoposition geoposition = await geolocator.GetGeopositionAsync(
+            //         maximumAge: TimeSpan.FromMinutes(5),
+            //         timeout: TimeSpan.FromSeconds(10)
+            //        );
+
+            //    //With this 2 lines of code, the app is able to write on a Text Label the Latitude and the Longitude, given by {{Icode|geoposition}}
+            //    geolocation.Text = "GPS:" + geoposition.Coordinate.Latitude.ToString("0.0000000") + ", " + geoposition.Coordinate.Longitude.ToString("0.0000000");
+            //}
+            ////If an error is catch 2 are the main causes: the first is that you forgot to include ID_CAP_LOCATION in your app manifest. 
+            ////The second is that the user doesn't turned on the Location Services
+            //catch (UnauthorizedAccessException)
+            //{
+            //    //print exception: "location is disabled in phone settings"
+            //}
+
+            Geolocator geolocator = new Geolocator();
+            //geolocator.DesiredAccuracyInMeters = 50;
+            Geoposition geoposition = null;
             try
             {
-                Geoposition geoposition = await geolocator.GetGeopositionAsync(
-                     maximumAge: TimeSpan.FromMinutes(5),
-                     timeout: TimeSpan.FromSeconds(10)
-                    );
-
-                //With this 2 lines of code, the app is able to write on a Text Label the Latitude and the Longitude, given by {{Icode|geoposition}}
-                geolocation.Text = "GPS:" + geoposition.Coordinate.Latitude.ToString("0.0000000") + ", " + geoposition.Coordinate.Longitude.ToString("0.0000000");
+                geoposition = await geolocator.GetGeopositionAsync(
+                    maximumAge: TimeSpan.FromMinutes(5),
+                    timeout: TimeSpan.FromSeconds(10));
+  
+                geolocation.Text = "GPS:" + geoposition.Coordinate.Point.Position.Latitude.ToString("0.0000000") + ", " + geoposition.Coordinate.Point.Position.Longitude.ToString("0.0000000");
             }
-            //If an error is catch 2 are the main causes: the first is that you forgot to include ID_CAP_LOCATION in your app manifest. 
-            //The second is that the user doesn't turned on the Location Services
             catch (Exception ex)
             {
-                //exception
+                // Handle errors like unauthorized access to location
+                // services or no Internet access.
             }
+            myMapControl.Center = geoposition.Coordinate.Point;
+            myMapControl.ZoomLevel = 15;
         }
     }
 }
